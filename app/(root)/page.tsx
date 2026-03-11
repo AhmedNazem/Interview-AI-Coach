fcd0fefb9c37f6b9a14a509cf4ff691295a47db7
AhmedNazem <aahhaammdd55@gmail.com>
2026-03-11 23:20:34 +0300

    feat: Implement core UI for interview display including layout, home page, interview cards, and tech icons.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,54 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready With AI-Powered Practise and Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions and get instatnt Feedback
+          </p>
+          <Button
+            render={<Link href={"/interview"} />}
+            nativeButton={false}
+            className="btn-primary max-sm:w-full "
+          >
+            Start an Interview
+          </Button>
+        </div>
+        <Image
+          src={"/robot.png"}
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8 ">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          <p>You have not taken any interviews yet</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
